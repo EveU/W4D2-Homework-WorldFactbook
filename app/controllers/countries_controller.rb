4d594c3331ee464a1e7bbf022984c8fb64392877
Evelyn Utterson <e.utterson@hotmail.co.uk>
2015-12-08 21:52:47 +0000

    Adds links across site and form for adding new countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,14 @@
 class CountriesController < ApplicationController
   def index
+    @countries = Country.all.order(:name)
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  private
+  def country_params
+    params.require(:country).permit(:name, :image, :size, :population, :first_language, :national_symbol, :national_anthem)
   end
 end
